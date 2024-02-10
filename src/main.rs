75f26dcfe89e49cdd40c5e19e68cdf4e3bb79af9
João Delgado <joao.delgado@jumia.com>
2024-02-10 17:42:47 +0000

    Pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
+use anyhow::{Context, Result};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
 
-use anyhow::Result;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .context("creating TCP server")?;
 
-fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => loop {
-                let mut buf = [0; 1024];
-                let _ = stream.read(&mut buf)?;
-                stream.write_all(b"+PONG\r\n")?;
-            },
+    loop {
+        match listener.accept().await {
+            Ok((stream, _addr)) => {
+                tokio::spawn(async move {
+                    if let Err(e) = handle_stream(stream).await {
+                        println!("error handling client: {}", e);
+                    }
+                });
+            }
             Err(e) => {
-                println!("error: {}", e);
+                println!("error accepting connection: {}", e);
             }
         }
     }
+}
 
-    Ok(())
+async fn handle_stream(mut stream: TcpStream) -> Result<()> {
+    loop {
+        let mut buf = [0; 1024];
+        let _ = stream
+            .read(&mut buf)
+            .await
+            .context("read command from client")?;
+        stream
+            .write_all(b"+PONG\r\n")
+            .await
+            .context("write result to client")?;
+    }
 }
